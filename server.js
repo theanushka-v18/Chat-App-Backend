ce1fea1e4b5664f6c6f25999b42645ebc0190398
Anushka Verma <anushkaverma779@gmail.com>
2025-09-06 18:10:03 +0530

    updated frontend url

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const server = createServer(app); // ✅ create HTTP server
 // Middlewares
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://theanushka-chat-app.vercel.app",
     credentials: true,
   })
 );
@@ -32,7 +32,7 @@ app.use("/api", chatRouter);
 // ✅ Initialize Socket.IO
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "https://theanushka-chat-app.vercel.app",
     methods: ["GET", "POST"],
     credentials: true,
   },
